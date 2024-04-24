f2d28f36a5439dd559014dbae3ea6fd9851ec668
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-24 22:03:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+	let num = [0,1,2,3,4,5,6,7,8,9]
+	let alp = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+	let capal = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+	let arr = [num,alp,capal]
+	let str = ""
+	for(let i=0;i<l;i++){
+		let ind = Math.random()*3
+		let dig = arr[ind]
+		str+=dig[Math.random()*dig.length]
+	}
+	return str
 }
 
 // Do not change the code below.
