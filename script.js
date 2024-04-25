db48144b94c110fbfe4b985e437af8fda3fb2a4c
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-25 10:54:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function makeid(l) {
   // write your code here
-	let num = [0,1,2,3,4,5,6,7,8,9]
-	let alp = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
-	let capal = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
-	let arr = [num,alp,capal]
-	let str = ""
-	for(let i=0;i<l;i++){
-		let ind = parseInt(Math.random()*3)
-		let dig = arr[ind]
-		str+=dig[parseInt(Math.random()*dig.length)]
-	}
-	return str
+	let num = ['0','1','2','3','4','5','6','7','8','9'];
+	  let alp = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	  let capal = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+	  let arr = [num,alp,capal];
+	  let str = "";
+	  for(let i=0;i<l;i++){
+	    let dig = arr[parseInt(Math.random()*arr.length)];
+	    str+=dig[parseInt(Math.random()*dig.length)];
+	  }
+	  return str;
 }
 
 // Do not change the code below.
